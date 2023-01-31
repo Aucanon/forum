736d0d95a41f478d57e2329d6a081ecd2d2804e3
Aucanon <aucanon@163.com>
2023-01-31 11:37:08 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+import App from './App'
+import uView from 'uview-ui'
+
+// #ifndef VUE3
+import Vue from 'vue'
+Vue.config.productionTip = false
+Vue.use(uView)
+App.mpType = 'app'
+const app = new Vue({
+    ...App
+})
+// http拦截器，将此部分放在new Vue()和app.$mount()之间，才能App.vue中正常使用
+import httpInterceptor from '@/common/http.interceptor.js'
+Vue.use(httpInterceptor, app)
+
+// http接口API集中管理引入部分
+import httpApi from '@/common/http.api.js'
+Vue.use(httpApi, app)
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
